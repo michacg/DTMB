90c1fc5394616b54cbf3aeb181d11f6a422de31b
michacg <michacg2@uci.edu>
2019-01-23 16:00:03 -0800

    Working on main scene

---
+++ Don't Touch My Balls!/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetAxis("Horizontal") == 1)
+		{
+			gameObject.rigidbody.AddForce(Vector2(speed * Time.deltatime , 0));
+		}
+
+		if (Input.GetAxis("Horizontal") == -1)
+		{
+
+		}
+
+		if (input.GetAxis("Vertical") == 1)
+		{
+
+		}
+
+		if (Input.GetAxis("Horizontal") == -1)
+		{
+
+		}
+
+		
+	}
+}
