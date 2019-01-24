26fdf4ba52e0de6813270f9767817247f332c940
michacg <michacg2@uci.edu>
2019-01-23 16:37:14 -0800

    Fixed script

--- Don't Touch My Balls!/Assets/Scripts/PlayerController.cs
+++ Don't Touch My Balls!/Assets/Scripts/PlayerController.cs
@@ -2,36 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
+
+	public float speed = 5;
+
+	private Rigidbody2D rb;
+
 	void Start () 
 	{
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		Debug.Log(Input.GetAxis("Vertical"));
 		if (Input.GetAxis("Horizontal") == 1)
 		{
-			gameObject.rigidbody.AddForce(Vector2(speed * Time.deltatime , 0));
+			rb.AddForce( new Vector2(speed * Time.deltaTime , 0));
 		}
 
 		if (Input.GetAxis("Horizontal") == -1)
 		{
-
+			rb.AddForce( new Vector2(-speed * Time.deltaTime , 0));
 		}
 
-		if (input.GetAxis("Vertical") == 1)
+		if (Input.GetAxis("Vertical") > 1)
 		{
-
+			rb.AddForce( new Vector2(0 , speed * Time.deltaTime));
 		}
 
-		if (Input.GetAxis("Horizontal") == -1)
+		if (Input.GetAxis("Horizontal") < -1)
 		{
-
+			rb.AddForce( new Vector2(0 , -speed * Time.deltaTime));
 		}
 
 		
