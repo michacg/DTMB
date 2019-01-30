94f81f9f7998153a933380bafff36143f930e684
michacg <michacg2@uci.edu>
2019-01-30 15:09:35 -0800

    player controller

--- Don't Touch My Balls!/Assets/Scripts/PlayerController.cs
+++ Don't Touch My Balls!/Assets/Scripts/PlayerController.cs
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetAxis("Vertical") == 1)
 		{
-			rb.AddForce( new Vector2(0 , vertical_speed * Time.deltaTime));
+			rb.AddForce( new Vector2(0 , vertical_speed * Time.deltaTime))
 		}
 
 		if (Input.GetAxis("Vertical") == -1)
