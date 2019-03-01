b4a28434e04c627e75b67a1c967a2f3645a71ec1
jjlebow@gmail.com <jlebow@uci.edu>
2019-02-28 18:26:56 -0800

    added some audio stuff

--- Don't Touch My Balls!/Assets/Scripts/AudioManager.cs
+++ Don't Touch My Balls!/Assets/Scripts/AudioManager.cs
@@ -7,8 +7,10 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
+    private Sound s;
     public Sound[] sounds;
     public static AudioManager instance;
+    public bool flag1 = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -22,13 +24,13 @@ public class AudioManager : MonoBehaviour
         }
         
         DontDestroyOnLoad(gameObject);
-        foreach(Sound s in sounds)
+        foreach(Sound track in sounds)
         {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
+            track.source = gameObject.AddComponent<AudioSource>();
+            track.source.clip = track.clip;
+            track.source.volume = track.volume; 
+            track.source.pitch = track.pitch;
+            track.source.loop = track.loop;
         }
     }
     void Start()
@@ -37,9 +39,47 @@ public class AudioManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        s.source.volume = s.volume;
+        //if(!LevelManager.instance.isGameOver)
+        //{
+          //  if(!s.source.isPlaying)
+            //    Play("LoopTrack");
+
+        //}
+        //else
+        //{
+
+            if(s.name != "GameOverLoop")
+            {
+
+                if(s.source.volume > 0)
+                {
+                    s.volume -= Time.deltaTime / 2.5f;  //For a duration of fadeTime, volume gradually decreases till its 0
+                    //WaitForSeconds(0.1f);
+                    ///StartCoroutine(FadeOut(1f));
+                    //flag1 = true;
+                }
+                if(s.volume <= 0) 
+                    s.source.Stop();
+            }
+            if(!s.source.isPlaying)
+            {
+                s.source.volume = s.volume;
+                Play("GameOverLoop");
+                if(s.source.volume < 1)
+                {
+                    StartCoroutine(FadeIn(1f));
+                }
+            }
+
+       // }
+    }
+
     public void Play (string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s = Array.Find(sounds, sound => sound.name == name);
         if(s == null)
         {
             Debug.Log("ERROR: Sound not found");
@@ -47,4 +87,30 @@ public class AudioManager : MonoBehaviour
         }
         s.source.Play();
     }
+
+    public IEnumerator FadeOut(float fadeTime)
+    {
+        while(s.volume > 0)
+        {
+            s.volume -= Time.deltaTime / fadeTime;  //For a duration of fadeTime, volume gradually decreases till its 0
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public IEnumerator FadeIn(float fadeTime)
+    {
+        while (s.volume < 1)
+        {
+            s.volume += Time.deltaTime / fadeTime; //fades in over course of seconds fadeTime
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void stopAudio()
+    {
+        s.source.Stop();
+    }
+
+
+    
 }
--- Don't Touch My Balls!/Assets/Scripts/Sound.cs
+++ Don't Touch My Balls!/Assets/Scripts/Sound.cs
@@ -19,6 +19,8 @@ public class Sound
 
 
 
+
+
     // Start is called before the first frame update
     void Start()
     {
