010177fa7e5b27ed2cf95dc70041dead8d727a8a
jcvong <39819708+jcvong@users.noreply.github.com>
2019-03-06 15:39:48 -0800

    Adjusted UI text and size.

---
+++ Don't Touch My Balls!/Assets/Scripts/BobaMechanics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobaMechanics : MonoBehaviour
+{
+    public GameObject straw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (LevelManager.instance.vacuuming)
+            StrawVacuum();
+    }
+
+    void StrawVacuum()
+    {
+
+    }
+}
